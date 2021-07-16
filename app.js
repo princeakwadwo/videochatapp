0f4709aac771254671dce6ed2dc7ccf6c1d209d7
Prince K. Annie <prince.annie@persol.net>
2021-07-16 11:57:35 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+const server = require('http').Server(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
+app.set('view engine', 'ejs');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use('/peerjs', peerServer);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.param.room });
+});
+
+io.on('connection', (socket) => {
+  socket.on('join-room', (roomId, userId, userName) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    socket.on('message', (message) => {
+      io.to(roomId).emit('createMessage', message, userName);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 3030);
